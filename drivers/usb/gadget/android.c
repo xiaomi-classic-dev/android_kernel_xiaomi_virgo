12981c9371ae650cf309751fa024461686459667
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-22 19:46:27 +0200

    Revert "usb: gadget: Enable HID function for charging mode"
    This reverts commit 2a397b60665d5e1b8d1ecb044eb77ff79a65c52f.
    
    Revert "usb: gadget: Add file for USB HID function"
    
    This reverts commit 2aa1159f573e0d5dee4f8a9a3411acc56baa27d4.
    
    * No longer needed in M
    
    Change-Id: If6f95e9fff14268f3e47efc96bf26452ba6a306d

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -101,7 +101,6 @@ static int android_is_set_cdrom(void);
 #include "f_uac1.c"
 #endif
 #include "f_ncm.c"
-#include "f_charger.c"
 
 MODULE_AUTHOR("Mike Lockwood");
 MODULE_DESCRIPTION("Android Composite USB Driver");
@@ -1658,19 +1657,6 @@ static struct android_usb_function ccid_function = {
 	.bind_config	= ccid_function_bind_config,
 };
 
-/* Charger */
-static int charger_function_bind_config(struct android_usb_function *f,
-						struct usb_configuration *c)
-{
-	return charger_bind_config(c);
-}
-
-static struct android_usb_function charger_function = {
-	.name		= "charging",
-	.bind_config	= charger_function_bind_config,
-};
-
-
 static int
 mtp_function_init(struct android_usb_function *f,
 		struct usb_composite_dev *cdev)
@@ -2437,7 +2423,6 @@ static struct android_usb_function *supported_functions[] = {
 #ifdef CONFIG_SND_RAWMIDI
 	&midi_function,
 #endif
-	&charger_function,
 	NULL
 };
 
